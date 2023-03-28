71869bb4d4e9a678bd94f7c5be21584d553660c0
jmvigueras <jmvigueras@jmvigueras.com>
2023-03-28 10:09:40 +0200

    update Azure module vnet-spoke_v2

--- azure/vnet-spoke_v2/peering.tf
+++ azure/vnet-spoke_v2/peering.tf
@@ -2,7 +2,7 @@
 # Create peering to VNET FGT
 ######################################################
 resource "azurerm_virtual_network_peering" "peerSpoketoFGT-1" {
-  name                      = "peer-spoke-to-FGT-1"
+  name                      = "${var.prefix}-peer-to-FGT-1"
   resource_group_name       = var.resource_group_name
   virtual_network_name      = var.vnet_fgt["name"]
   remote_virtual_network_id = azurerm_virtual_network.vnet-spoke.id
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network_peering" "peerSpoketoFGT-1" {
 }
 
 resource "azurerm_virtual_network_peering" "peerSpoketoFGT-2" {
-  name                      = "peer-spoke-to-FGT-2"
+  name                      = "${var.prefix}-peer-to-FGT-2"
   resource_group_name       = var.resource_group_name
   virtual_network_name      = azurerm_virtual_network.vnet-spoke.name
   remote_virtual_network_id = var.vnet_fgt["id"]
