592023f10ef0592badac3716a9c24b59523e1a4a
jmvigueras <jmvigueras@jmvigueras.com>
2023-04-18 23:16:42 +0200

    update Azure module vnet-fgt fix typo variable accelerate

--- azure/vnet-fgt/vars.tf
+++ azure/vnet-fgt/vars.tf
@@ -34,12 +34,12 @@ variable "vnet-fgt_cidr" {
 // Make the the instance choosed supports accelerated networking.
 // Check: https://docs.microsoft.com/en-us/azure/virtual-network/accelerated-networking-overview#supported-vm-instances
 variable "accelerate" {
-  type    = bool
-  default = true
-  description   = "Boolean viriable to config accelerated interfaces"
+  type        = bool
+  default     = true
+  description = "Boolean viriable to config accelerated interfaces"
   validation {
-    condition = can(regex("^([t][r][u][e]|[f][a][l][s][e])$",var.accelerate))
-    error     = "accelerate must be either true or false."
+    condition     = can(regex("^([t][r][u][e]|[f][a][l][s][e])$", var.accelerate))
+    error_message = "accelerate must be either true or false."
   }
 }
 
